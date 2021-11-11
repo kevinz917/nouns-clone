b2aafaf3dd9b383cfe0e4af1cc7a865b07c4fdd5
Kevin Z <zkw917@gmail.com>
2021-11-10 22:17:00 -0500

    Mo functions

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.6;
 
 // TODO: Add non-reentrant guard
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -15,7 +16,7 @@ struct Auction {
   uint256 endTime;
 }
 
-contract AuctionHouse is OwnableUpgradeable, PausableUpgradeable {
+contract AuctionHouse is OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable {
   IToken public nft; // nft address
 
   uint256 public tokenId;
@@ -28,12 +29,18 @@ contract AuctionHouse is OwnableUpgradeable, PausableUpgradeable {
   event AuctionSettled(uint256 _tokenId, uint256 _amount, address _winner);
 
   constructor(IToken _nft, uint256 _auctionDuration) {
+    __ReentrancyGuard_init();
     __Pausable_init();
 
     _pause(); // TODO: Remove this?
 
     nft = _nft;
     auctionDuration = _auctionDuration;
+  }
+
+  // Admin sets duration
+  function setAuctionDuration(uint256 _duration) public onlyOwner {
+    auctionDuration = _duration;
   }
 
   /**
@@ -53,6 +60,8 @@ contract AuctionHouse is OwnableUpgradeable, PausableUpgradeable {
 
     currentBidEvent.amount = msg.value;
     currentBidEvent.bidder = payable(msg.sender);
+
+    // extend time
 
     emit Bid(msg.sender, msg.value, tokenId);
   }
@@ -78,25 +87,36 @@ contract AuctionHouse is OwnableUpgradeable, PausableUpgradeable {
    * @dev This accepts ETH
    */
   function _settleAuction() internal {
-    Auction memory _currentBidEvent = auctions[tokenId];
-    require(block.timestamp > _currentBidEvent.endTime, "AUCTION NOT ENDED");
+    Auction memory _currentAuction = auctions[tokenId];
 
-    if (_currentBidEvent.bidder == address(0)) {
+    require(_currentAuction.startTime != 0, "AUCTION HASN'T STARTED"); // Can't settle an un-started auction
+    require(block.timestamp >= _currentAuction.endTime, "AUCTION NOT ENDED");
+
+    if (_currentAuction.bidder == address(0)) {
       // burn
     } else {
-      nft.transferFrom(address(this), _currentBidEvent.bidder, tokenId);
+      nft.transferFrom(address(this), _currentAuction.bidder, tokenId);
     }
 
-    emit AuctionSettled(tokenId, _currentBidEvent.amount, _currentBidEvent.bidder);
+    emit AuctionSettled(tokenId, _currentAuction.amount, _currentAuction.bidder);
   }
 
   /**
    * @notice Change to new auction
    * @dev This accepts ETH
    */
-  function advanceAuction() public whenNotPaused {
+  function advanceAuction() public whenNotPaused nonReentrant {
     _settleAuction();
     _createAuction();
+  }
+
+  /**
+     * @notice Settles the current auction. Probably used during emergency?
+     * @dev This function can only be called when the contract is paused.
+     Does this mean anyone can settle an auction?
+     */
+  function settleAuction() public nonReentrant whenPaused {
+    _settleAuction();
   }
 
   // // This was ripped off from NounsDAO. TODO: Study this
